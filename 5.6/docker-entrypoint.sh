3b5c5d58a9615b8bcb75802c1bb4debf0d5b8196
Pavel Prischepa <pprischepa@gmail.com>
2016-09-23 19:33:34 +0700

    Updated default xdebug config

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -10,4 +10,16 @@ if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
      sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php5/conf.d/xdebug.ini
 fi
 
+if [ "$PHP_XDEBUG_AUTOSTART" -eq "0" ]; then
+     sed -i 's/^xdebug.remote_autostart.*/xdebug.remote_autostart = 0/' /etc/php5/conf.d/xdebug.ini
+fi
+
+if [ "$PHP_XDEBUG_REMOTE_CONNECT_BACK" -eq "0" ]; then
+     sed -i 's/^xdebug.remote_connect_back.*/xdebug.remote_connect_back = 0/' /etc/php5/conf.d/xdebug.ini
+fi
+
+if [ -n "$PHP_XDEBUG_REMOTE_HOST" ]; then
+     sed -i 's/^xdebug.remote_host.*/'"xdebug.remote_host = ${PHP_XDEBUG_REMOTE_HOST}"'/' /etc/php5/conf.d/xdebug.ini
+fi
+
 exec php-fpm
