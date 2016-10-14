e04050d079c3db4d614c8a344c54eafcac490919
Pavel Prischepa <pprischepa@gmail.com>
2016-10-14 19:35:08 +0700

    Test integration with travis

--- scripts/run-tests.sh
+++ scripts/run-tests.sh
@@ -3,7 +3,7 @@
 set -ev
 
 docker ps | grep -q php
-docker run --user=82 php -v | grep "PHP $TEST_PHP"
+docker run --user=82 php php -v | grep "PHP $TEST_PHP"
 docker run --user=82 php pwd | grep "/var/www/html"
 docker run --user=82 php composer --version | grep "1.2.1"
 docker run --user=82 php drush version | grep "8.1.5"
