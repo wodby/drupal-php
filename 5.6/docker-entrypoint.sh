62f080ceda9e5413f9dc891e9fe6018854ed3612
Pavel Prischepa <pprischepa@gmail.com>
2016-10-18 02:12:19 +0700

    Added ability to map ssh keys

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 
 if [ ! -d /mnt/ssh ]; then
     mkdir -p /home/www-data/.ssh
-    cp /mnt/ssh/* > /home/www-data/.ssh/
+    cp /mnt/ssh/* /home/www-data/.ssh/
     chown -R www-data:www-data /home/www-data/.ssh
     chmod -R 700 /home/www-data/.ssh
 fi
