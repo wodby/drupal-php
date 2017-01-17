0c9430af69c69139a6efe95ad47ff1b836009d18
Pavel Prischepa <pprischepa@gmail.com>
2017-01-17 09:52:14 +0700

    Migrated to Alpine 3.5

--- 5.6/docker-entrypoint.sh
+++ 5.6/docker-entrypoint.sh
@@ -6,23 +6,30 @@ if [[ $DEBUG ]]; then
   set -x
 fi
 
-if [ -n "$PHP_SENDMAIL_PATH" ]; then
+if [ -d /mnt/ssh ]; then
+    mkdir -p /home/www-data/.ssh
+    cp /mnt/ssh/* /home/www-data/.ssh/
+    chown -R www-data:www-data /home/www-data/.ssh
+    chmod -R 700 /home/www-data/.ssh
+fi
+
+if [[ $PHP_SENDMAIL_PATH ]]; then
      sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /etc/php5/php.ini
 fi
 
-if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
+if [[ $PHP_XDEBUG_ENABLED = 1 ]]; then
      sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php5/conf.d/xdebug.ini
 fi
 
-if [ "$PHP_XDEBUG_AUTOSTART" -eq "0" ]; then
+if [[ $PHP_XDEBUG_AUTOSTART = 0 ]]; then
      sed -i 's/^xdebug.remote_autostart.*/xdebug.remote_autostart = 0/' /etc/php5/conf.d/xdebug.ini
 fi
 
-if [ "$PHP_XDEBUG_REMOTE_CONNECT_BACK" -eq "0" ]; then
+if [[ $PHP_XDEBUG_REMOTE_CONNECT_BACK = 0 ]]; then
      sed -i 's/^xdebug.remote_connect_back.*/xdebug.remote_connect_back = 0/' /etc/php5/conf.d/xdebug.ini
 fi
 
-if [ -n "$PHP_XDEBUG_REMOTE_HOST" ]; then
+if [ $PHP_XDEBUG_REMOTE_HOST ]; then
      sed -i 's/^xdebug.remote_host.*/'"xdebug.remote_host = ${PHP_XDEBUG_REMOTE_HOST}"'/' /etc/php5/conf.d/xdebug.ini
 fi
 
--- 7.0/docker-entrypoint.sh
+++ 7.0/docker-entrypoint.sh
@@ -17,19 +17,19 @@ if [ -n "$PHP_SENDMAIL_PATH" ]; then
      sed -i 's@^;sendmail_path.*@'"sendmail_path = ${PHP_SENDMAIL_PATH}"'@' /etc/php7/php.ini
 fi
 
-if [ "$PHP_XDEBUG_ENABLED" -eq "1" ]; then
+if [[ $PHP_XDEBUG_ENABLED = 1 ]]; then
      sed -i 's/^;zend_extension.*/zend_extension = xdebug.so/' /etc/php7/conf.d/00_xdebug.ini
 fi
 
-if [ "$PHP_XDEBUG_AUTOSTART" -eq "0" ]; then
+if [[ $PHP_XDEBUG_AUTOSTART = 0 ]]; then
      sed -i 's/^xdebug.remote_autostart.*/xdebug.remote_autostart = 0/' /etc/php7/conf.d/00_xdebug.ini
 fi
 
-if [ "$PHP_XDEBUG_REMOTE_CONNECT_BACK" -eq "0" ]; then
+if [[ $PHP_XDEBUG_REMOTE_CONNECT_BACK = 0 ]]; then
      sed -i 's/^xdebug.remote_connect_back.*/xdebug.remote_connect_back = 0/' /etc/php7/conf.d/00_xdebug.ini
 fi
 
-if [ -n "$PHP_XDEBUG_REMOTE_HOST" ]; then
+if [[ $PHP_XDEBUG_REMOTE_HOST ]]; then
      sed -i 's/^xdebug.remote_host.*/'"xdebug.remote_host = ${PHP_XDEBUG_REMOTE_HOST}"'/' /etc/php7/conf.d/00_xdebug.ini
 fi
 
