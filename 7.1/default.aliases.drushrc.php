bdd440d5d980b1d9353d3e629eeafd1a03e25c78
Serge Skripchuk <s.skripchuk@internetdevels.com>
2017-01-18 12:14:15 +0200

    Added PHP 7.1 support

---
+++ 7.1/default.aliases.drushrc.php
@@ -0,0 +1,6 @@
+<?php
+
+$aliases[isset($_SERVER['PHP_SITE_NAME']) ? $_SERVER['PHP_SITE_NAME'] : 'dev'] = [
+  'root' => '/var/www/html/' . (isset($_SERVER['PHP_DOCROOT']) ? $_SERVER['PHP_DOCROOT'] : ''),
+  'uri' => isset($_SERVER['PHP_HOST_NAME']) ? $_SERVER['PHP_HOST_NAME'] : 'localhost:8000',
+];
