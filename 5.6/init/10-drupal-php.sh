89ad745635a6763d4969f8e3bd65211d33beb8c7
Chingis Sandanov <chingis@wodby.com>
2017-04-13 19:34:36 +0700

    Remove extra chown operations

--- 5.6/init/10-drupal-php.sh
+++ 5.6/init/10-drupal-php.sh
@@ -12,8 +12,6 @@ execTpl() {
     fi
 }
 
-chown www-data:www-data "${WODBY_DIR_FILES}"
-
 if [[ -n "${DRUPAL_VERSION}" ]]; then
     if [[ "${DRUPAL_VERSION}" == "7" ]] || [[ "${DRUPAL_VERSION}" == "8" ]]; then
         execTpl 'sites.php.tpl' "${WODBY_DIR_CONF}/wodby.sites.php"
