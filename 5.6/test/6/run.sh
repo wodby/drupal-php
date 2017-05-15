01a6f0dce163f4db3bb9fa1f7fcdf9cef16adb4a
Chingis Sandanov <chingis@wodby.com>
2017-05-15 14:25:52 +0700

    Use params for mariadb check-ready

--- 5.6/test/6/run.sh
+++ 5.6/test/6/run.sh
@@ -9,7 +9,7 @@ fi
 . ../../../test-images.env
 
 docker-compose up -d
-docker-compose exec mariadb make check-ready -f /usr/local/bin/actions.mk
+docker-compose exec mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
 docker-compose exec nginx make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec php apk add --update jq
 docker-compose exec --user=82 php tests.sh
