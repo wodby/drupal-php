6207b6e05e3dd79153643f08c1976f8512433d48
Chingis Sandanov <chingis@wodby.com>
2017-05-19 17:25:50 +0700

    Unfreeze

--- 7.0/test/8/run.sh
+++ 7.0/test/8/run.sh
@@ -3,13 +3,13 @@
 set -e
 
 if [[ -n "${DEBUG}" ]]; then
-  set -x
+    set -x
 fi
 
 . ../../../test-images.env
 
 docker-compose up -d
-docker-compose exec mariadb make check-ready -f /usr/local/bin/actions.mk max_try=12 wait_seconds=5
+docker-compose exec mariadb make check-ready max_try=12 wait_seconds=5 -f /usr/local/bin/actions.mk
 docker-compose exec nginx make check-ready -f /usr/local/bin/actions.mk
 docker-compose exec php apk add --update jq
 docker-compose exec --user=82 php tests.sh
