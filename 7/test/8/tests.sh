9a723a3209adabd1e5401fca33cc518a5ba7b8d5
Chingis Sandanov <chingis@wodby.com>
2018-01-31 18:52:17 +0700

    Do not test for drush version

--- 7/test/8/tests.sh
+++ 7/test/8/tests.sh
@@ -29,7 +29,7 @@ drupal | grep -q "Drupal Console"
 echo "OK"
 
 echo -n "Checking drush... "
-drush version --format=yaml | grep -q "Drush Version"
+drush version --format=yaml | grep -q "Core Drush commands"
 echo "OK"
 
 echo -n "Checking drush patchfile... "
