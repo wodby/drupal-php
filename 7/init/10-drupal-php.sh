907890bb41bdd995fa4686a83a76e70b2a23429d
Chingis Sandanov <chingis@wodby.com>
2018-02-08 13:54:48 +0700

    Fix config dir permissions for D8

--- 7/init/10-drupal-php.sh
+++ 7/init/10-drupal-php.sh
@@ -7,9 +7,14 @@ if [[ -n "${DEBUG}" ]]; then
 fi
 
 if [[ -n "${DRUPAL_VERSION}" ]]; then
-    if [[ "${DRUPAL_VERSION}" == "7" ]] || [[ "${DRUPAL_VERSION}" == "8" ]]; then
+    if [[ "${DRUPAL_VERSION}" == 7 ]] || [[ "${DRUPAL_VERSION}" == 8 ]]; then
         gotpl "/etc/gotpl/sites.php.tpl" > "${CONF_DIR}/wodby.sites.php"
     fi
 
     gotpl "/etc/gotpl/drupal${DRUPAL_VERSION}.settings.php.tpl" > "${CONF_DIR}/wodby.settings.php"
+
+    if [[ "${DRUPAL_VERSION}" == 8 ]]; then
+        mkdir "${FILES_DIR}/config"
+        chmod 775 "${FILES_DIR}/config"
+    fi
 fi
