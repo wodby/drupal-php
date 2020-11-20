f4a042f0edfe763e1e9a198abc6ea6006776d5f4
Chingis Sandanov <chingis@wodby.com>
2020-11-20 09:12:33 +0700

    Remove freeze of preserve path

--- 7/tests/7/tests.sh
+++ 7/tests/7/tests.sh
@@ -54,8 +54,6 @@ GIT_URL="https://github.com/drupal-composer/drupal-project.git"
 make git-clone url="${GIT_URL}" -f /usr/local/bin/actions.mk
 make git-checkout target=7.x -f /usr/local/bin/actions.mk
 
-# https://github.com/drupal-composer/preserve-paths/issues/28
-COMPOSER_MEMORY_LIMIT=-1 composer require drupal-composer/preserve-paths:dev-master
 COMPOSER_MEMORY_LIMIT=-1 composer install -n
 composer require drupal/varnish drupal/redis
 
