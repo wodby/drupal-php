b8c19d9ac5df1048e6b61318225443160104f511
Chingis Sandanov <chingis@wodby.com>
2021-06-03 22:26:37 +0700

    Push only non-dev amd64 images to registry.wodby.com

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -2,8 +2,12 @@
 
 set -e
 
+username="${1}"
+password="${2}"
+registry="${3}"
+
 if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]]; then
-    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}"
+    docker login -u "${username}" -p "${password}" ${registry}
 
     if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
       export STABILITY_TAG="${GITHUB_REF##*/}"
@@ -12,12 +16,6 @@ if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]];
     IFS=',' read -ra tags <<< "${TAGS}"
 
     for tag in "${tags[@]}"; do
-        make buildx-push TAG="${tag}";
-    done
-
-    docker login -u "${WODBY1_REGISTRY_USERNAME}" -p "${WODBY1_REGISTRY_PASSWORD}" registry.wodby.com
-
-    for tag in "${tags[@]}"; do
-        make buildx-push TAG="${tag}" REGISTRY="registry.wodby.com";
+        make buildx-push TAG="${tag}" REGISTRY=${registry};
     done
 fi
