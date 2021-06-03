e2eba53b67d163029a0ad6f1a5cf0a0c68c9be37
Chingis Sandanov <chingis@wodby.com>
2021-06-03 22:37:50 +0700

    Fixes

--- .github/actions/release.sh
+++ .github/actions/release.sh
@@ -2,12 +2,8 @@
 
 set -e
 
-username="${1}"
-password="${2}"
-registry="${3}"
-
 if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]]; then
-    docker login -u "${username}" -p "${password}" ${registry}
+    docker login -u "${DOCKER_USERNAME}" -p "${DOCKER_PASSWORD}" ${DOCKER_REGISTRY}
 
     if [[ "${GITHUB_REF}" == refs/tags/* ]]; then
       export STABILITY_TAG="${GITHUB_REF##*/}"
@@ -16,6 +12,6 @@ if [[ "${GITHUB_REF}" == refs/heads/master || "${GITHUB_REF}" == refs/tags/* ]];
     IFS=',' read -ra tags <<< "${TAGS}"
 
     for tag in "${tags[@]}"; do
-        make buildx-push TAG="${tag}" REGISTRY=${registry};
+        make buildx-push TAG="${tag}" REGISTRY=${DOCKER_REGISTRY};
     done
 fi
