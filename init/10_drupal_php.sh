b671d6f7e581aab9a92dc60233c725b8df56e933
Chingis <csandanov@users.noreply.github.com>
2022-12-20 19:01:48 +0700

    Update 10_drupal_php.sh

--- init/10_drupal_php.sh
+++ init/10_drupal_php.sh
@@ -10,7 +10,7 @@ if [[ -n "${DRUPAL_VERSION}" ]]; then
     gotpl "/etc/gotpl/sites.php.tmpl" > "${CONF_DIR}/wodby.sites.php"
     gotpl "/etc/gotpl/drupal${DRUPAL_VERSION}.settings.php.tmpl" > "${CONF_DIR}/wodby.settings.php"
 
-    if [[ "${DRUPAL_VERSION}" == 8 || "${DRUPAL_VERSION}" == 9 ]]; then
+    if [[ -n "${DRUPAL_FILES_SYNC_SALT}" ]]; then
         sudo init_container "${FILES_DIR}/config/sync_${DRUPAL_FILES_SYNC_SALT}"
     fi
 fi
